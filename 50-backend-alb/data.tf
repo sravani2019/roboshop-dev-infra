2d10b99d620132518cf6da83e4c878e8db3ec7ef
sravani192019 <sravani.jamithireddy@gmail.com>
2026-06-30 17:16:38 +0530

    changes

--- 50-backend-alb/data.tf
+++ 50-backend-alb/data.tf
@@ -1,33 +1,10 @@
-data "aws_ssm_parameter" "mongodb_sg_id" {
-    name = "/${var.project}/${var.environment}/mongodb_sg_id"
-}
-data "aws_ssm_parameter" "redis_sg_id" {
-    name = "/${var.project}/${var.environment}/redis_sg_id"
 
-}
-data "aws_ssm_parameter" "rabbitmq_sg_id" {
-    name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
-}
 
-data "aws_ssm_parameter" "mysql_sg_id" {
-    name = "/${var.project}/${var.environment}/mysql_sg_id"
+data "aws_ssm_parameter" "backed_lb_sg_id" {
+    name = "/${var.project}/${var.environment}/backend_lb_sg_id"
 }
-data "aws_ssm_parameter" "database_subnet_ids" {
+data "aws_ssm_parameter" "private_subnet_id" {
     name = "/${var.project}/${var.environment}/database_subnet_id"
 }
 
-data "aws_ami" "joindevops" {
-  most_recent = true
-  owners = ["973714476881"]
 
-  filter {
-    name   = "name"
-    values = ["Redhat-9-DevOps-Practice"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
- 
-}
