987322a02be0774609c7bc0383807418e3b086d2
sravani192019 <sravani.jamithireddy@gmail.com>
2026-07-01 03:53:58 +0530

    initial commit

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -49,3 +49,39 @@ resource "aws_ami_from_instance" "catalogue" {
     local.common_tags
   )
 }
+resource "aws_launch_template" "catalogue" {
+  name = "${local.common_name}-catalogue"
+  image_id = aws_ami_from_instance.catalogue.id
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.catalogue_sg_id]
+  update_default_version = true
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = merge(
+    {
+      Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+    },
+    local.common_tags
+  )
+  }
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = merge(
+    {
+      Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+    },
+    local.common_tags
+  )
+  }
+  #launch template tags
+  tags = merge(
+    {
+      Name = "${local.common_name}-catalogue-${var.app_version}-${aws_instance.catalogue.id}"
+    },
+    local.common_tags
+  )
+}
