96d10c244cb099578d373a5e62761a838fd85b6f
sravani192019 <sravani.jamithireddy@gmail.com>
2026-06-30 17:13:32 +0530

    changes

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -8,31 +8,3 @@ locals {
     }
     common_name = "${var.project}-${var.environment}"
 }
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.front_end.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/http"
-      message_body = "<h1>Hi,Iam from HTTP Backend ALB</h1>"
-      status_code  = "200"
-    }
-  }
-}
-
-
-resource "aws_route53_record" "www" {
-  zone_id = var.zone_id
-  name    = "*.backend-lb-${var.environment}.devops90s.space"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.backend_lb.dns_name
-    zone_id                = aws_lb.backend_lb.zone_id
-    evaluate_target_health = true
-  }
-}
