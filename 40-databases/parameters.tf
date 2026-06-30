2c74bee451fb0df22bf77d6ccbbbc74a8fe117b6
sravani192019 <sravani.jamithireddy@gmail.com>
2026-06-30 14:09:25 +0530

    changes

--- 40-databases/parameters.tf
+++ 40-databases/parameters.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "mysql_pass" {
-  count = length(var.sg_names)
+ 
   name  = "/${var.project}/${var.environment}/mysql_root_password"
   type  = "SecureString"
   value = var.mysql_root_password
