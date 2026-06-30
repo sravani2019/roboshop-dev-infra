b7479cc417d80f3008d36a99bcd9229c7ce0f55f
sravani192019 <sravani.jamithireddy@gmail.com>
2026-06-30 14:12:47 +0530

    changes

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -8,9 +8,14 @@ data "aws_ssm_parameter" "redis_sg_id" {
 data "aws_ssm_parameter" "rabbitmq_sg_id" {
     name = "/${var.project}/${var.environment}/rabbitmq_sg_id"
 }
+
+data "aws_ssm_parameter" "mysql_sg_id" {
+    name = "/${var.project}/${var.environment}/mysql_sg_id"
+}
 data "aws_ssm_parameter" "database_subnet_ids" {
     name = "/${var.project}/${var.environment}/database_subnet_id"
 }
+
 data "aws_ami" "joindevops" {
   most_recent = true
   owners = ["973714476881"]
