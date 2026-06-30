c61d9a01798b32c77a780ebceddcfd7ef91d8e93
sravani192019 <sravani.jamithireddy@gmail.com>
2026-06-30 17:19:42 +0530

    changes

--- 50-backend-alb/data.tf
+++ 50-backend-alb/data.tf
@@ -1,6 +1,6 @@
 
 
-data "aws_ssm_parameter" "backed_lb_sg_id" {
+data "aws_ssm_parameter" "backend_lb_sg_id" {
     name = "/${var.project}/${var.environment}/backend_lb_sg_id"
 }
 data "aws_ssm_parameter" "private_subnet_id" {
