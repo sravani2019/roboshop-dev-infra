a1fb59b30a34384ca0c0ac83b80172c915058fc7
sravani192019 <sravani.jamithireddy@gmail.com>
2026-07-02 11:36:37 +0530

    autoscale commit

--- 70-acm/variables.tf
+++ 70-acm/variables.tf
@@ -10,9 +10,10 @@ variable "environment" {
 variable "app_version" {
     default = "v4"
 }
-# variable "zone_id" {
-#   default = "Z05085342KJ3YDJ9HCQQS"
-# }
+
+variable "zone_id" {
+  default = "Z05085342KJ3YDJ9HCQQS"
+}
 
 variable "domain-name" {
   default = "devops90s.space"
