73281fb962f05083b21fe04489b1b65f6efc57fb
sravani192019 <sravani.jamithireddy@gmail.com>
2026-07-02 09:41:40 +0530

    autoscale commit

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -179,15 +179,15 @@ resource "aws_lb_listener_rule" "catalogue" {
   }
 }
 
-# resource "terraform_data" "catalogue_delete" {
-#   triggers_replace = [
-#     aws_instance.catalogue.id
-#   ]
+resource "terraform_data" "catalogue_delete" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
   
-#   depends_on = [ aws_autoscaling_policy.catalogue ]
-#   provisioner "local-exec" {
-#     command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
+  depends_on = [ aws_autoscaling_policy.catalogue ]
+  provisioner "local-exec" {
+    command = "aws ec2 terminate-instances --instance-ids ${aws_instance.catalogue.id}"
     
-#   }
+  }
    
-# }
+}
